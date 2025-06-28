fc9bf57f7e499eb274b3b69328d702c72f3a2ba0
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-06-28 12:07:00 +0530

    X-treme level question 39

---
+++ Type 1-Code Comprehension/Extreme Level/Question39.py
@@ -0,0 +1,12 @@
+def square(x):
+    print(x * x)
+
+res = square(3)
+print(res)
+
+# A) 9 None   ✅
+# B) 9 9
+# C) None 9
+# D) 9
+
+# ✅ Explanation: `square` prints but returns None → `print(res)` → None
